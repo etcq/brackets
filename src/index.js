f5f752043d466e1b02b99fa50d96c181aae216b4
Anton Sushnikov <resonans93@gmail.com>
2024-08-13 06:59:32 +0500

    start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let counter = 0
+  for (let i of str) {
+    bracketsConfig.map(arr => {
+      if (arr[0] === i) {
+        counter ++;
+      } else if (arr[1] === i) {
+        counter --;
+      }
+    })
+
+    if (counter < 0) {
+      return false
+    }
+  }
+
+    return counter === 0
+  
 }
