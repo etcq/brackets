a91672bfda011289e6440240c241f6a97ca67e3d
Anton Sushnikov <resonans93@gmail.com>
2024-08-14 04:04:59 +0500

    finish

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  let counter = 0
-  for (let i of str) {
-    bracketsConfig.map(arr => {
-      if (arr[0] === i) {
-        counter ++;
-      } else if (arr[1] === i) {
-        counter --;
+  let stack = []
+  let checking = true
+  for (let i = 0; i < str.length; i++) {
+    const curr = str[i];
+    if ((curr === '|' || curr == '7' || curr == '8') && stack[stack.length - 1] === curr) {
+        stack.pop()
+    } else {
+      bracketsConfig.forEach(element => {
+      if (curr == element[0]) {
+        stack.push(curr)
+      } else if (curr == element[1]) {
+        if (element[0] !== stack.pop()) {
+          checking = false;
+        }
       }
-    })
-
-    if (counter < 0) {
+    });  
+    }
+    
+    if (checking === false) {
       return false
     }
   }
-
-    return counter === 0
+  return stack.length === 0;
   
 }
